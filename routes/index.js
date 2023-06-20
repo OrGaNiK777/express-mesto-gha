56b3008efaf15cf52811d9694183c3b6f3a5cc79
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-06-20 20:59:31 +0500

    20.06.2023 dom v22

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const cardsRoutes = require('./cards');
 const usersRoutes = require('./users');
 
-router.get('/404', (req, res) => {
+router.use('*', (req, res) => {
   res.status(404);
-  return res.send({ message: 'Воу воу полегче' });
+  return res.send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 router.use(cardsRoutes);
