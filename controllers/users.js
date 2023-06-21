a0e73104430b3242b8208dbaab31704e9aca936e
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-06-21 22:12:08 +0500

    21.06.2023 dom v9

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,13 @@ const BadRequestError = require('../errors/bad-request-error');// 400
 const getUsers = (req, res) => User.find({}).then((user) => res.status(200).send(user));
 
 const getUsersById = (req, res, next) => {
-  const { id } = req.params;
+  const { id } = req.params.id;
   User.findById(id)
     .then((user) => {
       res.status(200).send(user);
-      if (!user) { throw new NotFoundError(`Пользователь c id: ${id} не найден`); }
     })
     .catch((err) => {
+      if (!User) { throw new NotFoundError(`Пользователь c id: ${id} не найден`); }
       if (err.name === 'CastError') {
         throw new BadRequestError('Переданы некорректные данные');
       }
