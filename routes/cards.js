6ffe9d813c84b0c7a270ee42084710f3acccdf9a
OrGaNiK777 <OrGaNiK777@yandex.ru>
2023-07-05 15:57:45 +0500

    05.07.2023 dom v19

--- routes/cards.js
+++ routes/cards.js
@@ -28,19 +28,19 @@ router.post('/cards', celebrate({
 
 router.delete('/cards/:id', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().hex().length(24).replace(),
+    id: Joi.string().hex().length(24).required(),
   }),
 }), deleteCardById);
 
 router.delete('/cards/:id/likes', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().hex().length(24).replace(),
+    id: Joi.string().hex().length(24).required(),
   }),
 }), deleteLikesCardById);
 
 router.put('/cards/:id/likes', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().hex().length(24).replace(),
+    id: Joi.string().hex().length(24).required(),
   }),
 }), putLikesCardById);
 
